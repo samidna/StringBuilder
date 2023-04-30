185ea73fc3002e10860fc03dfd2027cb8588c43c
SamidAghayev <samidna@code.edu.az>
2023-04-30 19:12:00 +0400

    created StringBuilder Custom

---
+++ Models/Builders.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+
+namespace ConsoleApp9.Models;
+
+internal class Builders
+{
+    private char[] _arr;
+    private int _capacity = 16;
+    private int _length = 0;
+    public int Length
+    {
+        get
+        {
+            return _length;
+        }
+        set
+        {
+            if (value < _length)
+            {
+                throw new Exception($"Length must be more than {_length}");
+            }
+            if (value > _capacity && value <= 2 * _capacity)
+            {
+                Capacity *= 2;
+            }
+            if (value > 2 * _capacity)
+            {
+                Capacity = value;
+            }
+            _length = value;
+
+        }
+    }
+    public int Capacity
+    {
+        get
+        {
+            return _capacity;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("value cannot be less than zero");
+            }
+            if (value < Length)
+            {
+                throw new Exception($"Capacity must be more than {Length}");
+            }
+            _capacity = value;
+            Array.Resize(ref _arr, _capacity);
+        }
+    }
+    public void CustomBuilder()
+    {
+        _arr = new char[_capacity];
+    }
+    public void Append(char ch)
+    {
+        if (Length == Capacity)
+        {
+            Capacity = Capacity * 2;
+        }
+        _arr[Length] = ch;
+        Length++;
+    }
+    public void ReplaceChar(char oldchar, char newchar)
+    {
+        for (int i = 0; i < Length; i++)
+        {
+            if (_arr[i] == oldchar)
+            {
+                _arr[i] = newchar;
+            }
+        }
+    }
+    public void RemoveIndex(int index, int count)
+    {
+        char[] array = new char[Length];
+        int j = 0;
+        if (index < 0 || index >= Length)
+        {
+            throw new ArgumentOutOfRangeException("Your index is out of range");
+        }
+        for (int i = 0; i < Length; i++)
+        {
+            if (i < index || i >= index + count)
+            {
+                array[j] = _arr[i];
+                j++;
+            }
+        }
+        _arr = array;
+    }
+    public char[] GetResult()
+    {
+        return _arr;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp9.Models;
+using System.Net.Security;
+using System.Reflection.Emit;
+using System.Text;
+
+Builders sb = new Builders();
+sb.CustomBuilder();
+sb.Append('h');
+sb.Append('e');
+sb.Append('l');
+sb.Append('l');
+sb.Append('o');
+sb.Append('w');
+sb.Append('o');
+sb.Append('r');
+sb.Append('l');
+sb.Append('d');
+sb.ReplaceChar('w', 'r');
+sb.RemoveIndex(4, 1);
+Console.WriteLine(sb.GetResult());
+
+
+
